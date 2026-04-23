c6ba021038afa927d113e18e099db73c4c887be2
TadaEXE <36825573+TadaEXE@users.noreply.github.com>
2026-04-23 07:20:12 +0200

    did not mean to remove cmakelists

---
+++ test/runtime/model/graph_test.cpp
@@ -0,0 +1,16 @@
+#include "runtime/model/graph.hpp"
+
+#include <array>
+#include <list>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "runtime/model/edge.hpp"
+#include "runtime/model/node.hpp"
+#include "runtime/model/port.hpp"
+namespace {
+  TEST(Graph, None) {
+    SUCCEED();
+  }
+}  // namespace
